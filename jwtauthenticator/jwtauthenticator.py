7d1a8646aa91293cfdeae72e90576e2b1fb31a56
Dan Stieglitz <dstieglitz@stainless.ai>
2023-07-11 12:19:20 -0700

    refactoring logging methods, additional logs

--- jwtauthenticator/jwtauthenticator.py
+++ jwtauthenticator/jwtauthenticator.py
@@ -17,28 +17,6 @@ from urllib import parse
 
 class JSONWebTokenLoginHandler(BaseHandler):
 
-    @staticmethod
-    def print_exception():
-        import sys
-        import traceback
-        exc_type, exc_value, exc_tb = sys.exc_info()
-        except_string = traceback.format_exception(exc_type, exc_value, exc_tb)
-        print(except_string)
-        return except_string
-
-    def log_create(self):
-        if not self.authenticator.debug:
-            return
-        logr = open('app.log', 'a')
-        exceptions = print_exception()
-        logr.write(f"Failed to load: {str(exceptions)}\n")
-
-    def log_text(self, text):
-        if not self.authenticator.debug:
-            return
-        logr = open('app.log', 'a')
-        logr.write(f"Log: {str(text)}\n")
-
     async def get(self):
         header_name = self.authenticator.header_name
         cookie_name = self.authenticator.cookie_name
@@ -47,8 +25,8 @@ class JSONWebTokenLoginHandler(BaseHandler):
         auth_header_content = self.request.headers.get(header_name, "") if header_name else None
         auth_cookie_content = self.get_cookie(cookie_name, "") if cookie_name else None
         auth_param_content = self.get_argument(param_name, default="") if param_name else None
-        self.log_text('param_name:"' + str(param_name) + '"')
-        self.log_text('auth_param_content:' + str(auth_param_content))
+        self.authenticator.log_text('param_name:"' + str(param_name) + '"')
+        self.authenticator.log_text('auth_param_content:' + str(auth_param_content))
 
         signing_certificate = self.authenticator.signing_certificate
         secret = self.authenticator.secret
@@ -69,7 +47,7 @@ class JSONWebTokenLoginHandler(BaseHandler):
                 auth_param_content = parse.parse_qs(parse.urlparse(next_url).query).get(param_name, "")
                 if isinstance(auth_param_content, list):
                     auth_param_content = auth_param_content[0]
-                self.log_text('auth_param_content:' + str(auth_param_content))
+                self.authenticator.log_text('auth_param_content:' + str(auth_param_content))
 
         if auth_url and retpath_param:
             auth_url += ("{prefix}{param}=https://{host}{url}".format(
@@ -80,7 +58,7 @@ class JSONWebTokenLoginHandler(BaseHandler):
             ))
 
         if bool(auth_header_content) + bool(auth_cookie_content) + bool(auth_param_content) > 1:
-            self.log_text('auth_failed, multiple tokens')
+            self.authenticator.log_text('auth_failed, multiple tokens')
             raise web.HTTPError(400)
         elif auth_header_content:
             token = auth_header_content
@@ -88,26 +66,26 @@ class JSONWebTokenLoginHandler(BaseHandler):
             token = auth_cookie_content
         elif auth_param_content:
             token = auth_param_content
-            self.log_text('token:' + str(token))
+            self.authenticator.log_text('token:' + str(token))
         else:
-            self.log_text('auth_failed, no token')
+            self.authenticator.log_text('auth_failed, no token')
             return self.auth_failed(auth_url)
 
         try:
             if secret:
                 claims = self.verify_jwt_using_secret(token, secret, algorithms, audience)
-                self.log_text('claims from secret:' + str(claims))
+                self.authenticator.log_text('claims from secret:' + str(claims))
             elif signing_certificate:
                 claims = self.verify_jwt_with_claims(token, signing_certificate, algorithms, audience)
-                self.log_text('claims from signing_certificate:' + str(claims))
+                self.authenticator.log_text('claims from signing_certificate:' + str(claims))
             else:
-                self.log_text('auth_failed, no way to verify token')
+                self.authenticator.log_text('auth_failed, no way to verify token')
                 return self.auth_failed(auth_url)
         except jwt.exceptions.InvalidTokenError:
             return self.auth_failed(auth_url)
 
         username = self.retrieve_username(claims, username_claim_field, extract_username=extract_username)
-        self.log_text('username: ' + str(username))
+        self.authenticator.log_text('username: ' + str(username))
         user = await self.auth_to_user({'name': username})
         self.set_login_cookie(user)
 
@@ -120,19 +98,19 @@ class JSONWebTokenLoginHandler(BaseHandler):
             raise web.HTTPError(401)
 
     def verify_jwt_with_claims(self, token, signing_certificate, algorithms, audience):
-        self.log_text('verify_jwt_with_claims: algorithms=' + str(algorithms))
+        self.authenticator.log_text('verify_jwt_with_claims: algorithms=' + str(algorithms))
         opts = {}
         if not audience:
             opts = {"verify_aud": False}
         with open(signing_certificate, 'r') as rsa_public_key_file:
-            self.log_text('found signing_certificate')
+            self.authenticator.log_text('found signing_certificate')
             try:
                 dec = jwt.decode(token, rsa_public_key_file.read(), algorithms=algorithms, audience=audience,
                                  options=opts)
-                self.log_text('jwt = ' + str(dec))
+                self.authenticator.log_text('jwt = ' + str(dec))
                 return dec
             except Exception as e:
-                self.log_create()
+                self.authenticator.log_create()
                 raise e
 
     @staticmethod
@@ -152,6 +130,29 @@ class JSONWebTokenLoginHandler(BaseHandler):
 
 
 class JSONWebTokenAuthenticator(Authenticator):
+    @staticmethod
+    def print_exception():
+        import sys
+        import traceback
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        except_string = traceback.format_exception(exc_type, exc_value, exc_tb)
+        print(except_string)
+        return except_string
+    
+    def log_create(self):
+        if not self.debug:
+            return
+        logr = open('app.log', 'a')
+        exceptions = print_exception()
+        logr.write(f"Failed to load: {str(exceptions)}\n")
+    
+    def log_text(self, text):
+        if not self.debug:
+            return
+        logr = open('app.log', 'a')
+        logr.write(f"Log: {str(text)}\n")
+        print(f"Log: {str(text)}\n")
+    
     """
     Accept the authenticated JSON Web Token from header.
     """
@@ -231,9 +232,12 @@ class JSONWebTokenAuthenticator(Authenticator):
     def authenticate(self, *args):
         raise NotImplementedError()
 
-    # FIXME uncommenting this breaks authentication
-    # async def refresh_user(self, user, handler=None):
-    #     self.log_text("REFRESH_USER called")
+    async def refresh_user(self, user, handler=None):
+        self.log_text("REFRESH_USER called")
+        # auth_state = await user.get_auth_state()
+        # self.log_text(str(user))
+        # self.log_text(str(auth_state))
+        return True
 
 
 class JSONWebTokenLocalAuthenticator(JSONWebTokenAuthenticator, LocalAuthenticator):
